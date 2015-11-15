d5d224d1818a5ef5ed973d9b760f0363f7c7c74c
Daniel <edvinsson.d@gmail.com>
2015-11-15 20:30:28 +0100

    test

--- Uppgift 10/Form1.Designer.cs
+++ Uppgift 10/Form1.Designer.cs
@@ -40,7 +40,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Sitka Small", 24.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(132, 43);
+            this.label1.Location = new System.Drawing.Point(132, 25);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(211, 49);
             this.label1.TabIndex = 0;
